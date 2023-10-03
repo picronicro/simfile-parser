9da2ad68b08ad72c24a0c2e392f59131e4bb4ff7
picronicro <eliyaz2761@gmail.com>
2023-10-03 20:48:30 +0600

    it does work, but it skips other difficulties

--- project/main.js
+++ project/main.js
@@ -10,7 +10,7 @@ readBtn.addEventListener("click", () => {
     const reader = new FileReader()
     reader.onload = event => {
         const smContents = event.target.result
-        parseSimfile(smContents)
+        console.log(parseSimfile(smContents))
     }
     reader.readAsText(simfile)
 })
@@ -19,7 +19,10 @@ const regexpHeaderTags = /#([^:]+):([^;]+)/
 const regexpNotesTags = /^ {5}(.+?):/
 
 function parseSimfile(smContents) {
-    const simfileObject = {};
+    const simfileObject = {}; // base obj
+    simfileObject.headerTags = {} // init header tags obj
+    simfileObject.difficulties = {} // init difficulties obj
+
     const lines = smContents.split('\n');
 
     // first, parsing header tags
@@ -28,7 +31,7 @@ function parseSimfile(smContents) {
         if (!line.includes("NOTES")) {
             const formattedLine = regexpHeaderTags.exec(line)
             if (formattedLine) {
-                simfileObject[formattedLine[1].toLowerCase()] = formattedLine[2]
+                simfileObject.headerTags[formattedLine[1].toLowerCase()] = formattedLine[2]
             }
         } else {
             console.log("ended parsing header tags") // while selecting song, we have to read only header tags (no NOTES)
@@ -39,52 +42,69 @@ function parseSimfile(smContents) {
 
     // second, parse chart itself (#NOTES)
     console.log("parsing notes")
-    console.log("parsing NOTES tags")
     // we have to find index of line NOTES
     let startLine = 0
     for (let i = 0; i < lines.length; i++) {
-        // console.log(regexpNotesTags.exec(lines[i]))
-
         if (lines[i].includes("NOTES")) {
             startLine = i + 1
             break
         }
     }
-    // and then, iterate 5 times from that index to get all NOTES tags (like name, difficulty, groove radar, etc)
-    let notes = {}
-    let noteTagsIndex = 0
-    for (let i = startLine; i < startLine + 5 ; i++) {
-        noteTagsIndex ++
-        console.log(noteTagsIndex)
-        const line = regexpNotesTags.exec(lines[i])
+    // parse notes function
+    const diff = parseNotes(startLine, lines)
+    const diffName = diff.difficulty.toLowerCase()
 
-        switch (noteTagsIndex) {
-            case 1:
-                notes.chartType = line[1]
-                break
-            case 2:
-                notes.author = line[1]
-                break
-            case 3:
-                notes.difficulty = line[1]
-                break
-            case 4:
-                notes.meter = line[1]
-                break
-            case 5:
-                notes.grooveRadar = line[1]
-                break
-        }
-    }
-    simfileObject.difficulties = [notes]
-    console.log(simfileObject)
-
-    // now we have to parse notes till ";"
-    console.log("parsing notes")
+    simfileObject.difficulties[diffName] = diff
 
     return simfileObject;
 }
 
-function parseNotes() {
+function parseNotes(startLine, lines) {
+    // iterate 5 times from NOTES line index to get all NOTES tags (like name, difficulty, groove radar, etc)
+    let notesData = {}
+    let noteTagsIndex = 0 // max value 5, need for putting tags into notes object
+    for (let i = startLine; i < startLine + 5 ; i++) {
+        noteTagsIndex ++
+        const line = regexpNotesTags.exec(lines[i])
 
+        switch (noteTagsIndex) {
+            case 1:
+                notesData.chartType = line[1]
+                break
+            case 2:
+                notesData.author = line[1]
+                break
+            case 3:
+                notesData.difficulty = line[1]
+                break
+            case 4:
+                notesData.meter = line[1]
+                break
+            case 5:
+                notesData.grooveRadar = line[1]
+                break
+        }
+    }
+
+    // parse notes
+    let notes = [] // store all notes
+    let buffer = [] // store measure (pack of notes till ",")
+    for (let i = startLine + 5; i < lines.length; i++) {
+        if (!lines[i].includes(";")) {
+            if (!lines[i].includes(",")) {
+                buffer.push(lines[i].replace(/(\r\n|\n|\r)/gm, ""))
+            } else {
+                console.log("met comma, parse next measure")
+                notes.push(buffer)
+                buffer = []
+            }
+        } else {
+            console.log("notes parse end")
+            notes.push(buffer)
+            buffer = []
+        }
+    }
+    notesData.notes = notes
+
+    return notesData
 }
